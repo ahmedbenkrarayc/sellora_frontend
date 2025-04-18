13b3f0e5dc4f9fd5ec6de2243bf3ad7c01a7390a
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-18 00:30:52 +0000

    imported routes dynamically based on subdomain in main routes file

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import adminRoutes from './routes/admin'
+import landingRoutes from './routes/landing'
+import storeRoutes from './routes/store'
+
+const hostname = window.location.hostname
+const hostParts = hostname.split('.')
+
+let subdomain = ''
+if(hostParts.length > 2){
+  subdomain = hostParts[0].toLowerCase()
+}
+
+let activeRoutes = []
+
+if(subdomain === 'admin'){
+  activeRoutes = adminRoutes
+}else if(!subdomain){
+  activeRoutes = landingRoutes
+}else{
+  activeRoutes = storeRoutes
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  routes: activeRoutes
 })
 
 export default router
