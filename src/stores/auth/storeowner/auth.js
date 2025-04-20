fa90b40c074698bd5c0d87ab1da187bad394dc5c
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-20 03:57:46 +0000

    created login method in authstore

--- src/stores/auth/storeowner/auth.js
+++ src/stores/auth/storeowner/auth.js
@@ -2,12 +2,22 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 
 export const useAuthStore = defineStore('auth', () => {
+  const apiUrl = import.meta.env.VITE_API_URL
+
   const register = async (payload) => {
-    const apiUrl = import.meta.env.VITE_API_URL
-    return await axios.post(`${apiUrl}/register`, payload)
+    return await axios.post(`${apiUrl}/register`, payload, {
+      withCredentials: true
+    })
+  }
+
+  const login = async (payload) => {
+    return await axios.post(`${apiUrl}/login`, payload, {
+      withCredentials: true
+    })
   }
 
   return {
-    register
+    register,
+    login
   }
 })
