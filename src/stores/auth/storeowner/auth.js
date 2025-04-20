46a4b78096188844f6dc1b58054a3c4a805a899d
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-20 20:08:04 +0000

    created login with get current user actions with pinia

--- src/stores/auth/storeowner/auth.js
+++ src/stores/auth/storeowner/auth.js
@@ -1,8 +1,14 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
+import { ref } from 'vue'
+const user = ref(null)
 
 export const useAuthStore = defineStore('auth', () => {
   const apiUrl = import.meta.env.VITE_API_URL
+
+  const setUser = (data) => {
+    user.value = data
+  }
 
   const register = async (payload) => {
     return await axios.post(`${apiUrl}/register`, payload, {
@@ -11,13 +17,25 @@ export const useAuthStore = defineStore('auth', () => {
   }
 
   const login = async (payload) => {
-    return await axios.post(`${apiUrl}/login`, payload, {
+    const res = await axios.post(`${apiUrl}/login`, payload, {
       withCredentials: true
     })
+    setUser(res.data.user)
+    return res
+  }
+
+  const getUser = async () => {
+    const res = await axios.get(`${apiUrl}/user`, {
+      withCredentials: true
+    })
+    setUser(res.data)
+    return res
   }
 
   return {
+    user,
     register,
-    login
+    login,
+    getUser
   }
 })
