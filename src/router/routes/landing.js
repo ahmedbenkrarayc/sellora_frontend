796862e5506521ee5c69b4e67f0cbec9fe930616
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-20 20:09:16 +0000

    created a route for loginview in lading routes

--- src/router/routes/landing.js
+++ src/router/routes/landing.js
@@ -3,5 +3,10 @@ export default [
         path: '/register',
         name: 'RegisterStoreOwner',
         component: () => import('@/views/storeowner/auth/RegisterView.vue')
+    },
+    {
+        path: '/login',
+        name: 'LoginStoreOwner',
+        component: () => import('@/views/storeowner/auth/LoginView.vue')
     }
 ] 
