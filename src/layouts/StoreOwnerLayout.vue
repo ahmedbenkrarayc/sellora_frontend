e2a1a2e11c486f64932ceed837eee5f76dc37a93
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-21 11:13:39 +0000

    integrated components in StoreOwnerLayout

---
+++ src/layouts/StoreOwnerLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-[#F7F7F7] font-poppins flex flex-col h-screen overflow-hidden m-0 p-0">
+    <Header @toggle-menu="toggleMobileMenu" />
+    <MobileMenu :is-open="isMenuOpen" @close="toggleMobileMenu" />
+  
+    <main class="flex flex-1 overflow-hidden">
+      <SideBar />
+  
+      <div class="flex-1 overflow-y-auto p-4 md:p-8 pb-16">
+        <router-view />
+      </div>
+    </main>
+  </div>
+</template>
+  
+<script setup>
+import { ref } from 'vue'
+import Header from '@/components/storeowner/layout/Header.vue'
+import MobileMenu from '@/components/storeowner/layout/MobileMenu.vue'
+import SideBar from '@/components/storeowner/layout/SideBar.vue'
+
+const isMenuOpen = ref(false)
+
+const toggleMobileMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+  document.body.classList.toggle('overflow-hidden', isMenuOpen.value)
+}
+</script>
+  
+<style scoped>
+
+</style>
+  
