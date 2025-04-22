4265ff6769d70015f17729f8d8ec6339c22d91d5
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-22 11:03:58 +0000

    created a reusable component to display welcoming message based on which page

---
+++ src/components/commondashboards/WelcomeMessage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1 class="font-semibold text-[18px] md:text-[20px] text-[#222222]">Welcome back, {{ uppercaseFirstLetter(authStore.user?.fname) }}</h1>
+        <h2 class="text-[13px] md:text-[14px] text-[#636363]">{{ description }}</h2>
+    </div>
+</template>
+
+<script setup>
+import { useAuthStore } from '@/stores/auth/storeowner/auth'
+import { useStringUtils } from '@/composables/useStringUtils'
+const authStore = useAuthStore()
+const { uppercaseFirstLetter } = useStringUtils()
+
+const { description } = defineProps(['description'])
+</script>
