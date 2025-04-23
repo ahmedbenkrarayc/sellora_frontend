4e7378bb422313f5c1b8cd4e024e49a9d36dd1ad
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-23 01:20:58 +0000

    loading store data in app mounted when a store is visited

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { onMounted } from 'vue'
+import { useStoreStore } from '@/stores/store'
+const hostname = window.location.hostname
+const hostParts = hostname.split('.')
+
+const storeStore = useStoreStore()
+
+onMounted(async () => {
+  let subdomain = ''
+  if (hostParts.length > 2) {
+    subdomain = hostParts[0].toLowerCase()
+  }
+  
+  if (subdomain && subdomain !== 'admin') {
+    await storeStore.getStoreBySubdomain(subdomain)
+  }
+})
 </script>
 
 <template>
