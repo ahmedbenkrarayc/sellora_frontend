56d812c499cfde5eed8f1c5f12fa097072ac039c
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-25 10:22:22 +0000

    created product pinia store with create product action

---
+++ src/stores/storeowner/product.js
@@ -0,0 +1,110 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import axios from 'axios'
+
+export const useProductStore = defineStore('product', () => {
+  const apiUrl = import.meta.env.VITE_API_URL
+  const loading = ref(false)
+  const error = ref(null)
+
+  const createFullProduct = async (productPayload) => {
+    loading.value = true
+    error.value = null
+
+    try {
+      const productRes = await axios.post(
+        `${apiUrl}/products`,
+        {
+          title: productPayload.title,
+          description: productPayload.description,
+          baseprice: productPayload.baseprice,
+          subcategory_id: productPayload.subcategory_id,
+        },
+        { withCredentials: true }
+      )
+      const productId = productRes.data.id
+
+      for (const detail of productPayload.details) {
+        await axios.post(
+          `${apiUrl}/productdetails`,
+          {
+            key: detail.key,
+            value: detail.value,
+            product_id: productId,
+          },
+          { withCredentials: true }
+        )
+      }
+
+      const sizeIds = []
+      for (const size of productPayload.sizes) {
+        const sizeRes = await axios.post(
+          `${apiUrl}/sizes`,
+          {
+            name: size,
+            product_id: productId,
+          },
+          { withCredentials: true }
+        )
+        sizeIds.push({'id': sizeRes.data.data.id, 'name': sizeRes.data.data.name})
+      }
+
+      const colorIds = []
+      for (const color of productPayload.colors) {
+        const colorRes = await axios.post(
+          `${apiUrl}/colors`,
+          {
+            name: color,
+            product_id: productId,
+          },
+          { withCredentials: true }
+        )
+        colorIds.push({'id': colorRes.data.data.id, 'name': colorRes.data.data.name})
+      }
+
+      for (const variant of productPayload.variants) {
+        const variantRes = await axios.post(
+          `${apiUrl}/productvariants`,
+          {
+            name: productPayload.title+' '+variant.size+'-'+variant.color,
+            stock: variant.stock,
+            price: variant.price,
+            available: variant.availability,
+            product_id: productId,
+            size_id: sizeIds.find(item => item.name == variant.size)?.id,
+            color_id: colorIds.find(item => item.name == variant.color)?.id
+          },
+          { withCredentials: true }
+        )
+
+        const variantId = variantRes.data.id
+
+        for (const image of variant.images) {
+          const formData = new FormData()
+          formData.append('file', image)
+          formData.append('productvariant_id', variantId)
+
+          await axios.post(`${apiUrl}/productvariantimages`, formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+            withCredentials: true,
+          })
+        }
+      }
+
+      loading.value = false
+      return productRes.data
+    } catch (err) {
+      loading.value = false
+      error.value = err.response?.data?.message || 'Something went wrong'
+      throw err
+    }
+  }
+
+  return {
+    loading,
+    error,
+    createFullProduct,
+  }
+})
