7404c2969b09e5c4c747f2b0deb3f7797231d4e5
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-27 21:07:11 +0000

    implemented WelcomeMessage component in update product view

--- src/views/storeowner/product/UpdateProductView.vue
+++ src/views/storeowner/product/UpdateProductView.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="flex-1 overflow-y-auto p-4 md:p-8 pb-16">
     <Toast ref="toast" />
-    <h1 class="font-semibold text-[18px] md:text-[20px] text-[#222222]">Welcome back, Ahmed</h1>
-    <h2 class="text-[13px] md:text-[14px] text-[#636363]">Update your products effortlessly and stay ahead!</h2>
+    <WelcomeMessage description="Update your products effortlessly and stay ahead!" />
     
     <section class="w-full rounded-lg p-3 md:p-4 mt-4 md:mt-6">
       <div v-if="productStore.loading" class="flex justify-center items-center py-12">
@@ -466,6 +465,7 @@
 
 <script setup>
 import { ref, computed, watch, onMounted, reactive } from 'vue'
+import WelcomeMessage from '@/components/commondashboards/WelcomeMessage.vue'
 import Dialog from 'primevue/dialog'
 import Toast from 'primevue/toast'
 import { useSubcategoryStore } from '@/stores/storeowner/subcategory'
