90f2807bd95fac53e3f6fed55dd08955f5bf4f23
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-29 08:43:53 +0000

    created custom screen sizes in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,14 @@ export default {
           pearl: '#FAFAFA',   // Light background
         },
       },
+      screens: {
+        xs: '0',
+        sm: '350px',//640px
+        md: '750px',//768
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      }
     },
   },
   plugins: [],
