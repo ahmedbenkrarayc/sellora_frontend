0f6407d12e184ee34fb984a80fb79c05316cbc04
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-29 11:08:58 +0000

    added quote and feature components to homeview

--- src/views/store/HomeView.vue
+++ src/views/store/HomeView.vue
@@ -1,11 +1,15 @@
 <template>
     <Nav />
     <Header />
+    <OurFeatures />
     <Categories />
+    <QuoteSection />
 </template>
 
 <script setup>
 import Header from '@/components/store/home/Header.vue'
 import Nav from '@/components/store/home/Nav.vue'
 import Categories from '@/components/store/home/Categories.vue'
+import OurFeatures from '@/components/store/home/OurFeatures.vue'
+import QuoteSection from '@/components/store/home/QuoteSection.vue'
 </script>
