fa6c08ed5e668b31e746551f01b41efb64abaa92
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-29 15:19:18 +0000

    added footer and newsletter to homeview

--- src/views/store/HomeView.vue
+++ src/views/store/HomeView.vue
@@ -4,6 +4,10 @@
     <OurFeatures />
     <Categories />
     <QuoteSection />
+    <UniqueProducts />
+    <UniqueProducts />
+    <SubscribeNews />
+    <Footer />
 </template>
 
 <script setup>
@@ -12,4 +16,7 @@ import Nav from '@/components/store/home/Nav.vue'
 import Categories from '@/components/store/home/Categories.vue'
 import OurFeatures from '@/components/store/home/OurFeatures.vue'
 import QuoteSection from '@/components/store/home/QuoteSection.vue'
+import UniqueProducts from '@/components/store/home/UniqueProducts.vue'
+import SubscribeNews from '@/components/store/home/SubscribeNews.vue'
+import Footer from '@/components/store/home/Footer.vue'
 </script>
