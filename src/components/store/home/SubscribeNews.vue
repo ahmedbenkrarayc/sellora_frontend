76ce0081140bd0a1148236c7f9c4fad461567ff4
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-29 15:18:14 +0000

    created subscribe to newsletter component for home view

---
+++ src/components/store/home/SubscribeNews.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="mt-[20px] bg-white px-4 mb-10 pt-14 pb-16 text-center" >
+        <h1 class="sm:text-[30px] md:text-[35px] mt-2 font-[500]">Subscribe to our newsletter to get updates <br class="sm:hidden md:block"/>to our latest products</h1>
+        <p class="text-[#424242] font-poppins mt-2 sm:text-[14px] md:text-[16px]">Stay informed and inspired with our exclusive newsletter</p>
+        <div class="mt-8">
+            <div class="sm:block md:flex items-center justify-center">
+                <div class="flex bg-white border-[1px] rounded-lg">
+                    <div class="flex items-center justify-center py-2 px-4 text-gray-400">
+                    <i class="fas fa-envelope"></i>
+                    </div>
+                    <input type="email" placeholder="Enter your email" class="w-80 bg-white text-gray-700 py-2 px-4 rounded-r-lg focus:outline-none" />
+                </div>
+                <button class="block bg-black sm:mt-2 md:mt-0 sm:w-full md:w-auto text-white md:ml-2 py-2 px-8 rounded-lg hover:bg-gray-900 focus:outline-none">Subscribe</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
