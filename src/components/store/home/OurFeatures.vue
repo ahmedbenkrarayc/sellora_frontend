7df027336e77d4d15867f2a02c120e0d4f454153
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-29 11:08:30 +0000

    created features component for home view

---
+++ src/components/store/home/OurFeatures.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="mt-[80px] px-4">
+        <h1 class="toremove font-inter font-bold sm:text-[24px] sm:mb-3 md:text-[30px] text-center">We Provide <br/>Best Costumer Experiences.</h1>
+        <div class="w-full grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 max-w-[1317px] mx-auto">
+            <div class="flex items-center font-poppins mx-auto">
+                <div class="w-16 h-16 bg-[#f3f3f3] rounded-full flex items-center justify-center text-[20px] mr-4">
+                    <i aria-hidden="true" class="fa-solid fa-at"></i>
+                </div>
+                <div>
+                    <h5 class="text-lg">Best online support</h5>
+                    <p class="text-sm">24/7 availability and assistance</p>
+                </div>
+            </div>
+            <div class="flex items-center font-poppins mx-auto">
+                <div class="w-16 h-16 bg-[#f3f3f3] rounded-full flex items-center justify-center text-[20px] mr-4">
+                    <i aria-hidden="true" class="fa-solid fa-credit-card"></i>
+                </div>
+                <div>
+                    <h5 class="text-lg">Super Secure</h5>
+                    <p class="text-sm">Secure payments made simple</p>
+                </div>
+            </div>
+            <div class="flex items-center font-poppins mx-auto">
+                <div class="w-16 h-16 bg-[#f3f3f3] rounded-full flex items-center justify-center text-[20px] mr-4">
+                    <i aria-hidden="true" class="fa-solid fa-truck-fast"></i>
+                </div>
+                <div>
+                    <h5 class="text-lg">Best Quality</h5>
+                    <p class="text-sm">Unmatched excellence in quality</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
