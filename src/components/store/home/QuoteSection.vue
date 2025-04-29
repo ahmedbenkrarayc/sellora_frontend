c859e1d3c0ea40c8d26511036f2fe6bf4c0c25bf
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-29 11:06:39 +0000

    created QuoteSection component for home view

---
+++ src/components/store/home/QuoteSection.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="mt-[100px] object-cover w-full py-[70px] plx px-4 bg-black text-center text-white font-poppins font-bold">
+            <p class="text-[40px]"><i class="fa-solid fa-quote-left"></i></p>
+            <h1 class="text-[30px]">Be a yardstick of quality. Some people aren't used to an environment where excellence is expected.</h1>
+            <p class="text-[20px] mt-4">-Steve Jobs</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .plx{
+        background-image: url('https://images.pexels.com/photos/8133879/pexels-photo-8133879.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-color: rgba(0, 0, 0, 0.247);
+        background-blend-mode: multiply;
+        background-position: center;
+    }
+</style>
