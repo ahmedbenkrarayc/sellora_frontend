02172168fdfe93c8c3b1cf27a7400c443023c7c9
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-29 08:50:46 +0000

    created homeview component with nav header and categories section

---
+++ src/views/store/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <Nav />
+    <Header />
+    <Categories />
+</template>
+
+<script setup>
+import Header from '@/components/store/home/Header.vue'
+import Nav from '@/components/store/home/Nav.vue'
+import Categories from '@/components/store/home/Categories.vue'
+</script>
