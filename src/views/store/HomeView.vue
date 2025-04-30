808e6fdea4403909c873d03d2375ff52e3174eaa
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-30 09:02:27 +0000

    removed nav & footer from homeview

--- src/views/store/HomeView.vue
+++ src/views/store/HomeView.vue
@@ -1,5 +1,4 @@
 <template>
-    <Nav />
     <Header />
     <OurFeatures />
     <Categories />
@@ -7,16 +6,13 @@
     <UniqueProducts />
     <UniqueProducts />
     <SubscribeNews />
-    <Footer />
 </template>
 
 <script setup>
 import Header from '@/components/store/home/Header.vue'
-import Nav from '@/components/store/home/Nav.vue'
 import Categories from '@/components/store/home/Categories.vue'
 import OurFeatures from '@/components/store/home/OurFeatures.vue'
 import QuoteSection from '@/components/store/home/QuoteSection.vue'
 import UniqueProducts from '@/components/store/home/UniqueProducts.vue'
 import SubscribeNews from '@/components/store/home/SubscribeNews.vue'
-import Footer from '@/components/store/home/Footer.vue'
 </script>
