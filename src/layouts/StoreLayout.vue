0ee73df2dc14c5afb62b3246b49b2e0121ba09a6
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-30 08:57:48 +0000

    created storelayout with nav & footer components

---
+++ src/layouts/StoreLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <Nav />
+    <router-view />
+    <Footer />
+</template>
+    
+<script setup>
+import Nav from '@/components/store/home/Nav.vue'
+import Footer from '@/components/store/home/Footer.vue'
+</script>
+
+<style scoped>
+
+</style>
+    
