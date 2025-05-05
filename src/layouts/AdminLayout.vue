ce71746d16b44bcb049417e6e212d9c100400c5a
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-05-05 01:15:14 +0000

    created adminlayout

---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="bg-[#F7F7F7] font-poppins flex flex-col h-screen overflow-hidden m-0 p-0">
+      <Header @toggle-menu="toggleMobileMenu" />
+      <MobileMenu :is-open="isMenuOpen" @close="toggleMobileMenu" />
+    
+      <main class="flex flex-1 overflow-hidden">
+        <SideBar />
+    
+        <router-view />
+      </main>
+    </div>
+</template>
+    
+<script setup>
+import { ref } from 'vue'
+import Header from '@/components/admin/layout/Header.vue'
+import MobileMenu from '@/components/admin/layout/MobileMenu.vue'
+import SideBar from '@/components/admin/layout/SideBar.vue'
+
+const isMenuOpen = ref(false)
+
+const toggleMobileMenu = () => {
+    isMenuOpen.value = !isMenuOpen.value
+    document.body.classList.toggle('overflow-hidden', isMenuOpen.value)
+}
+</script>
+    
+<style scoped>
+  
+</style>
+    
